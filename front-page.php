fa143c357fd911e50ccdcda631047d91bf4e5fc9
Schurpf Mac <michael@schurpf.com>
2014-06-12 13:28:28 -0500

    add helper function to display hisrc, add sortable weight to determine portfolio

--- front-page.php
+++ front-page.php
@@ -12,89 +12,109 @@
  * @since      1.0.0
  */
 
-add_action('genesis_before_content', 'gmfb_portfolio');
 
-function gmfb_portfolio(){
+// Helper functions
+/**
+ * return an image from attachment ID wrapped in HISRC markup
+ *
+ * @param string  $link          url
+ * @param string  $width_mobile  number
+ * @param string  $height_mobile numer
+ * @return string                html
+ */
+function get_hisrc_img( $img_id, $link='', $width_mobile='200', $height_mobile='200' ) {
+  $out = '';
+  if ( $img_id !='' ) {
+    $img_src_mobile = wp_get_attachment_image_src( $img_id, array( $width_mobile, $height_mobile ) );
+    $img_src_desktop = wp_get_attachment_image_src( $img_id, array( 400, 400 ) );
+    $out .= '<div class="hisrc">';
+    if ( $link != '' ) {
+      $out .= '<a href="'.$link.'"><img src="'.$img_src_mobile[0].'" width="'.$width_mobile.'" height="'.$height_mobile.'"
+              data-1x="'.$img_src_desktop[0].'" alt="'.$post->post_title.'" /></a>';
+    } else {
+      $out .= '<img src="'.$img_src_mobile[0].'" width="'.$width_mobile.'" height="'.$height_mobile.'"
+              data-1x="'.$img_src_desktop[0].'" alt="'.$post->post_title.'" />';
+    }
+    $out .= '</div>';
+  }
+  return $out;
+}
+
+/**
+ * Get portfolio image marked up in HISRC markup
+ *
+ * @param string  $link          url
+ * @param string  $width_mobile  number
+ * @param string  $height_mobile number
+ * @return string                html
+ */
+function get_hisrc_img_portfolio( $link='', $width_mobile='200', $height_mobile='200' ) {
+  global $post;
+  $img_id = get_field( 'portfolio_logo', $post->ID );
+  return get_hisrc_img( $img_id, $link, $width_mobile, $height_mobile );
+}
+
+add_action( 'genesis_before_content', 'gmfb_portfolio' );
+
+function gmfb_portfolio() {
 ?>
-<h2>Current Projects</h2>
-<div class="row">
-        
-          <?php 
-          $args = array( 'post_type' => 'portfolio', 'posts_per_page' => 3 );
-          $loop = new WP_Query( $args );
-          $count = 0;
-          while ( $loop->have_posts() ) : $loop->the_post(); 
-          $count = $count + 1;
-          if ($count >2){ ?>
-            <div class="hidden-xs hidden-sm col-sm-6 col-md-4"> 
+
+  <?php
+  $args = array(  'post_type' => 'portfolio', 
+                  'posts_per_page' => 3, 
+                  'meta_key' => 'portfolio_weight',
+                  'orderby' => 'meta_value', 
+                  'order' => 'ASC' );
+  $loop = new WP_Query( $args );
+  $count = 0;
+  while ( $loop->have_posts() ) : $loop->the_post();
+  $count = $count + 1;
+  if ($count == 1){
+    ?>
+      <h2>Portolio</h2>
+      <div class="row">
+    <?php 
+  }
+
+  if ( $count >2 ) { ?>
+            <div class="hidden-xs hidden-sm col-sm-6 col-md-4">
           <?php }else { ?>
             <div class="col-sm-6 col-md-4">
           <?php }?>
-          <?php $portfolio_url = get_field('portfolio_link');
-          if ($portfolio_url == ''){
-            $portfolio_url = '#';
-          }?>
+          <?php $portfolio_url = get_field( 'portfolio_link' );
+  if ( $portfolio_url == '' ) {
+    $portfolio_url = '#';
+  }?>
 
           <a href="<?php echo $portfolio_url;?>"><h3><?php the_title(); ?></h3></a>
-          <?php $portfolio_img_id = get_field('portfolio_logo');
-          if ($portfolio_img_id !='')
-          {
-            $img_src_mobile = wp_get_attachment_image_src($portfolio_img_id, array(200,200));
-            $img_src_desktop = wp_get_attachment_image_src($portfolio_img_id, array(400,400));?>
-          <div class="hisrc">
-            <a href="<?php echo $portfolio_url;?>"><img src="<?php echo $img_src_mobile[0]; ?>" width="200" height="200"
-            data-1x="<?php echo $img_src_desktop[0]; ?>"
-            alt="<?php the_title(); ?>" /></a>
-          </div>
-          <?php } ?>
-          <?php if( get_field('portfolio_date') ): ?>
-            <p class="portfolio-date"><?php the_field('portfolio_date'); ?></p>
+          <?php echo get_hisrc_img_portfolio( $portfolio_url ); ?>
+          <?php if ( get_field( 'portfolio_date' ) ): ?>
+            <p class="portfolio-date"><?php the_field( 'portfolio_date' ); ?></p>
           <?php endif; ?>
-          <?php if( get_field('portfolio_description') ): ?>
-            <p class="portfolio-description"><?php the_field('portfolio_description');?></p>
+          <?php if ( get_field( 'portfolio_description' ) ): ?>
+            <p class="portfolio-description"><?php the_field( 'portfolio_description' );?></p>
           <?php endif; ?>
           </div>
           <?php endwhile;?>
-        </div>
-        <div class="row">
-         <div class="col-lg-4">
-          <h3>Lorem ipsum dolor sit.</h3>
-          <div class="hisrc">
-            <img src="http://upload.wikimedia.org/wikipedia/commons/6/6f/Earth_Eastern_Hemisphere.jpg" width="200" height="200"
-            data-1x="http://placehold.it/400x400.png"
-            data-2x="http://placehold.it/800x800.png"
-            alt="Celebrating Halloween in style" />
-          </div>
-          <p class="text-danger">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, eius, debitis ipsam dolor dolores atque cum reprehenderit nisi omnis aperiam.</p>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-primary" href="#" role="button">View details >></a></p>
-        </div>
-        <div class="col-lg-4">
-          <h3>Heading</h3>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-primary" href="#" role="button">View details >></a></p>
-       </div>
-        <div class="col-lg-4">
-          <h3>Heading</h3>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-          <p><a class="btn btn-primary" href="#" role="button">View details >></a></p>
-        </div>
-      </div>
-<?php
-remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
-add_action( 'genesis_sidebar', 'gmfb_do_sidebar' );
+
+          <?php if ($count>0){
+            echo '</div>';
+          } ?>
+  <?php
+  remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+  add_action( 'genesis_sidebar', 'gmfb_do_sidebar' );
 
 }
 
-function gmfb_do_sidebar(){
+function gmfb_do_sidebar() {
 
-echo genesis_html5() ? '<section class="widget widget_text">' : '<div class="widget widget_text">';
-	echo '<div class="widget-wrap">';
-?>	
-		<p>Sidebar gmfb</p>
+  echo genesis_html5() ? '<section class="widget widget_text">' : '<div class="widget widget_text">';
+  echo '<div class="widget-wrap">';
+?>
+    <p>Sidebar gmfb</p>
 <?php
-	echo '</div>';
-	echo genesis_html5() ? '</section>' : '</div>';	
+  echo '</div>';
+  echo genesis_html5() ? '</section>' : '</div>';
 
 }
 
--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 //* Start the engine
-include_once( get_template_directory() . '/lib/init.php' );
+include_once get_template_directory() . '/lib/init.php';
 
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'Genesis - Mobile First' );
@@ -21,22 +21,22 @@ add_theme_support( 'genesis-footer-widgets', 3 );
 
 
 //Register JS
-add_action('wp_enqueue_scripts', 'gmfb_enque_scripts');
-function gmfb_enque_scripts(){
+add_action( 'wp_enqueue_scripts', 'gmfb_enque_scripts' );
+function gmfb_enque_scripts() {
     wp_enqueue_script( 'gmfb_hisrc', get_stylesheet_directory_uri() . '/js/hisrc/hisrc.js', array( 'jquery' ), '', true );
     wp_enqueue_script( 'gmfb_js', get_stylesheet_directory_uri() . '/js/gmfb.js', array( 'jquery' ), '', true );
-    wp_enqueue_script('bootstrap_js_transition', get_stylesheet_directory_uri() . '/js/transition.js');
-    wp_enqueue_script('bootstrap_js_collapse', get_stylesheet_directory_uri() . '/js/collapse.js');
+    wp_enqueue_script( 'bootstrap_js_transition', get_stylesheet_directory_uri() . '/js/transition.js' );
+    wp_enqueue_script( 'bootstrap_js_collapse', get_stylesheet_directory_uri() . '/js/collapse.js' );
 }
 
 // Remove Gensis Admin Menu (http://genesistutorials.com/define-genesis-default-options/)
-remove_theme_support('genesis-admin-menu');
+remove_theme_support( 'genesis-admin-menu' );
 
 /** Remove Header */
 remove_action( 'genesis_header', 'genesis_do_header' );
 
 //Bootstrap  navigation
-require_once('wp_bootstrap_navwalker.php');
+require_once 'wp_bootstrap_navwalker.php';
 remove_action( 'genesis_after_header', 'genesis_do_nav' );
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
 
@@ -54,30 +54,30 @@ function gmfb_do_nav(  ) {
                 <span class="icon-bar"></span>
             </button>
 
-            <a class="navbar-brand logo" href="<?php bloginfo('url'); ?>">
-                <?php bloginfo('name'); ?>
+            <a class="navbar-brand logo" href="<?php bloginfo( 'url' ); ?>">
+                <?php bloginfo( 'name' ); ?>
             </a>
         </div>
 
         <?php
-            wp_nav_menu( array(
-                'menu'              => 'primary',
-                'theme_location'    => 'primary',
-                'depth'             => 2,
-                'container'         => 'div',
-                'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
-                'menu_class'        => 'nav navbar-nav',
-                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                'walker'            => new wp_bootstrap_navwalker())
-            );
-        ?>
+    wp_nav_menu( array(
+            'menu'              => 'primary',
+            'theme_location'    => 'primary',
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
+            'menu_class'        => 'nav navbar-nav',
+            'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+            'walker'            => new wp_bootstrap_navwalker() )
+    );
+?>
     </div>
 </nav>
 <?php
 
-/** Remove Title & Description */
-remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
-remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+    /** Remove Title & Description */
+    remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
+    remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
 
 
 }
@@ -85,29 +85,82 @@ remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
 
 //Temporary add in CPT (needs to go in plugin later)
 // define( 'ACF_LITE', true );
-include_once('advanced-custom-fields/acf.php');
- 
+include_once 'advanced-custom-fields/acf.php';
+
 add_action( 'init', 'gmfb_create_cpt' );
- 
+
 function gmfb_create_cpt() {
- 
-   $labels = array(
-    'name' => __( 'Portfolio' ),
-    'singular_name' => __( 'Portfolio' ),
-    'add_new_item' => 'Add title/heading'
+
+    $labels = array(
+        'name' => __( 'Portfolio' ),
+        'singular_name' => __( 'Portfolio' ),
+        'add_new_item' => 'Add title/heading'
     );
- 
+
     $args = array(
-    'labels' => $labels,
-    'public' => true,
-    'has_archive' => true,
-    'rewrite' => array('slug' => 'portfolio'),
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array( 'slug' => 'portfolio' ),
     );
- 
-  register_post_type( 'portfolio', $args);
+
+    register_post_type( 'portfolio', $args );
 }
 
-// if ( function_exists( 'add_image_size' ) ) { 
+/**
+ * Sortable Portoflio Column for weight
+ */
+add_action( 'admin_init', 'gmfb_portfolio_column' );
+function gmfb_portfolio_column() {
+    add_filter( 'manage_portfolio_posts_columns', 'gmfb_modify_user_table' );
+    add_action( 'manage_portfolio_posts_custom_column', 'gmfb_modify_user_table_row', 10 );
+
+    add_filter( 'manage_edit-portfolio_sortable_columns', 'cb_user_extra_sortable_cols' );
+    add_filter( 'request', 'cb_user_extra_orderby' );
+}
+
+function gmfb_modify_user_table( $columns ) {
+    $column_weight = array( 'portfolio_weight' => 'Weight' );
+
+    $columns = array_slice( $columns, 0, 2, true ) + $column_weight + array_slice( $columns, 2, NULL, true );
+
+    return $columns;
+}
+
+function gmfb_modify_user_table_row( $column, $post_id ) {
+    $custom_fields = get_post_custom( $post_id );
+
+    switch ( $column ) {
+    case 'portfolio_weight' :
+        echo $custom_fields['portfolio_weight'][0];
+        break;
+
+    default:
+    }
+
+}
+
+function cb_user_extra_sortable_cols( $columns ) {
+    //var_dump($columns);
+    $custom = array(
+        // meta column id => sortby value used in query
+        'portfolio_weight'    => 'portfolio_weight',
+    );
+    return wp_parse_args( $custom, $columns );
+}
+
+function cb_user_extra_orderby( $vars ) {
+    if ( isset( $vars['orderby'] ) && 'portfolio_weight' == $vars['orderby'] ) {
+        $vars = array_merge( $vars, array(
+                'meta_key' => 'portfolio_weight',
+                'orderby' => 'meta_value'
+            ) );
+    }
+    return $vars;
+}
+
+
+// if ( function_exists( 'add_image_size' ) ) {
 //     add_image_size( 'portfolio', ,300 ); //300 pixels wide (and unlimited height)
 //     add_image_size( 'homepage-thumb', 220, 180, true ); //(cropped)
 // }
